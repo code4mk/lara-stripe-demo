964e96fe829355d382d76814d9aeb7738aba52a1
Mostafa Kamal <radionishi@gmail.com>
2019-11-06 16:34:02 +0600

    sent payment request

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -93,6 +93,52 @@
         </style>
     </head>
     <body>
+
+        <!-- Load Stripe.js on your website. -->
+<script src="https://js.stripe.com/v3"></script>
+
+<!-- Create a button that your customers click to complete their purchase. Customize the styling to suit your branding. -->
+<button
+  style="background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em"
+  id="checkout-button-sku_G81IJuNDbYlJ42"
+  role="link"
+>
+  Checkout
+</button>
+
+<div id="error-message"></div>
+
+<script>
+(function() {
+  var stripe = Stripe('{{ $session->pkey }}');
+
+  var checkoutButton = document.getElementById('checkout-button-sku_G81IJuNDbYlJ42');
+  checkoutButton.addEventListener('click', function () {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe.redirectToCheckout({
+      items: [{sku: 'sku_G81IJuNDbYlJ42', quantity: 1}],
+
+      // Do not rely on the redirect to the successUrl for fulfilling
+      // purchases, customers may not always reach the success_url after
+      // a successful payment.
+      // Instead use one of the strategies described in
+      // https://stripe.com/docs/payments/checkout/fulfillment
+      successUrl: 'https://your-website.com/success',
+      cancelUrl: 'https://your-website.com/canceled',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+  });
+})();
+</script>
+
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
